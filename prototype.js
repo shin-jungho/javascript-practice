a2f1afd9d386425a652c961ffe0edd069fa13ad1
jungho shin <junghoshin@junghoui-MacBookAir.local>
2021-12-09 10:22:01 +0900

    prototype 실습

---
+++ prototype.js
@@ -0,0 +1,45 @@
+// let animal = {
+//   jumps: null
+// };
+
+// let rabbit = {
+//   __proto__:animal,
+//   jumps: true
+// };
+
+// console.log( rabbit.jumps );
+
+// delete rabbit.jumps;
+
+// console.log( rabbit.jumps );
+
+// delete animal.jumps;
+
+// console.log( rabbit.jumps );
+
+// pocket -> bed -> table -> head
+
+let head = {
+  glasses: 1
+};
+
+let table = {
+  pen: 3,
+  __proto__: head
+};
+
+let bed = {
+  sheet: 1, 
+  pillow: 2,
+  __proto__: table
+};
+
+
+let pockets = {
+  money: 2000,
+  __proto__: bed
+};
+
+console.log(pockets.pen);
+console.log(bed.glasses);
+console.log(table.money);
