e28d2bc7b61b6b75988bac1d07d3c920ceed669e
jungho shin <junghoshin@junghoui-MacBookAir.local>
2021-12-09 10:23:36 +0900

    prototype 실습

--- prototype.js
+++ prototype.js
@@ -43,3 +43,26 @@ let pockets = {
 console.log(pockets.pen);
 console.log(bed.glasses);
 console.log(table.money);
+
+let hamster = {
+  stomach: [],
+
+  eat(food) {
+    this.stomach.push(food);
+  }
+};
+
+let speedy = {
+  __proto__: hamster
+};
+
+let lazy = {
+  __proto__: hamster
+};
+
+// 햄스터 한 마리가 음식을 찾았습니다.
+speedy.eat("apple");
+alert( speedy.stomach ); // apple
+
+// 이 햄스터도 같은 음식을 가지고 있습니다. 왜 그럴까요? 고쳐주세요.
+alert( lazy.stomach ); // apple
